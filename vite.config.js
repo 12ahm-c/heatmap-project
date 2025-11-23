a9fff3db68f1a47cf88498df9c72e41c63c63105
ahmed <ahismail2005@gmail.com>
2025-11-23 10:24:57 +0100

    bismi ellah

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -5,5 +8,6 @@ export default defineConfig({
   },
   build: {
     outDir: "dist"
-  }
-})
+  },
+  base: "./"   // ← هذا مهم لتجنب 404 عند رفع المشروع
+});
